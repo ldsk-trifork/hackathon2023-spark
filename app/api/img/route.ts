24f3bd4c110803f9ab736fa2afae491bde4e2a14
Lasse D. Skaalum <ldsk@trifork.com>
2023-09-28 12:34:14 +0200

    Add todos

--- app/api/img/route.ts
+++ app/api/img/route.ts
@@ -1,5 +1,41 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function GET(req: NextRequest, res: NextResponse) {
-  return NextResponse.json({ message: "Hello, world!" });
+enum Priority {
+  LOW,
+  MEDIUM,
+  HIGH
+}
+
+enum Category {
+  CRACK,
+  SCRATCH,
+  DENT,
+  BURN,
+  OTHER
+}
+
+interface ReportData {
+  panelId: string
+  location: { lat: number, lng: number }
+  priority: Priority
+  category: Category
+  description: string
+}
+
+// Endpoint for receiving an image from the client
+export async function POST(req: NextRequest, res: NextResponse): Promise<ReportData> {
+  const blob = await req.blob()
+
+  // TODO: Lasse extract data from blob
+
+  // TODO: Jens use data from blob to call OpenAI API
+
+
+  return {
+    panelId: "123",
+    location: { lat: 0, lng: 0 },
+    priority: Priority.HIGH,
+    category: Category.CRACK,
+    description: "Big crack. Doesn't work."
+  }
 }
