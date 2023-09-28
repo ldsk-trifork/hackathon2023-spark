b933d7986bdfffaa3c4bcdbf13b3afa6e0faf17b
Jens Gram <jgm@trifork.com>
2023-09-28 16:04:46 +0200

    feat: Description completion

--- app/api/img/route.ts
+++ app/api/img/route.ts
@@ -1,29 +1,41 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Category, Priority } from "./types";
+import {NextRequest, NextResponse} from "next/server";
+import {Category, Prediction, Priority} from "./types";
 import getPredictionFromImage from "./getPredicitonFromImage";
-import { completion } from "../../service/openai";
+import {chat, Message} from "../../service/openai";
 
 // Endpoint for receiving an image from the client
 export async function POST(req: NextRequest, res: NextResponse) {
   const { image } = await req.json()
-  const prediction = getPredictionFromImage(image)
+  const prediction = getPredictionFromImage(image);
 
-  // TODO: Jens use data from blob to call OpenAI API
-
+  // const panelId = req.nextUrl.searchParams.get('panelId') as string;
+  const panelId = 'foo-panel';
+  const description = await generateDescription(panelId, prediction);
 
   return NextResponse.json({
     panelId: "123",
     location: { lat: 0, lng: 0 },
     priority: Priority.HIGH_IMPACT,
     category: Category.CRACK,
-    description: "Big crack. Doesn't work."
+    description
   })
 }
 
-// TODO Remove stupid impl.
-export async function GET(req: NextRequest): Promise<any> {
-    const prompt = req.nextUrl.searchParams.get('p') as string;
-    const text = await completion(prompt, Math.ceil(prompt.length / 4) + 100);
+async function generateDescription(panelId: string, predictions: Prediction[]): Promise<string> {
+    const probabilities = predictions
+        .sort((a, b) => b.confidence - a.confidence)
+        .map(p => `${Category[p.category].toLowerCase()}=${Math.round(p.confidence * 100)}%`)
+        .join(', ');
+    const messages: Message[] = [
+        {
+            role: 'system',
+            content: `As a solar panel technician your task is to write a short (100 words) incident description based on probable diagnoses for panel '${panelId}'`
+        },
+        {
+            role: 'user',
+            content: `The possible diagnoses and their respective probabilities are: ${probabilities}`
+        }
+    ];
 
-    return NextResponse.json(text);
+    return chat(messages, 200);
 }
--- app/service/openai.ts
+++ app/service/openai.ts
@@ -4,12 +4,27 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
-export async function completion(prompt: string, maxTokens: number) {
+export async function completion(prompt: string, maxResponseTokens: number) {
     const completion = await openai.completions.create({
         model: 'gpt-3.5-turbo-instruct',
         prompt,
-        max_tokens: maxTokens
+        max_tokens: maxResponseTokens
     });
 
     return completion.choices[0].text;
+}
+
+export type Message = {
+    role: 'system' | 'user',
+    content: string
+}
+
+export async function chat(messages: Message[], maxResponseTokens: number) {
+    const completion = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages,
+        max_tokens: maxResponseTokens
+    });
+
+    return completion.choices[0].message.content || '';
 }
