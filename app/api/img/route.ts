35b2efc14b3e10501bd9f18d015c3d54a46c49e6
Lasse D. Skaalum <ldsk@trifork.com>
2023-09-28 13:22:49 +0200

    Fix broken route

--- app/api/img/route.ts
+++ app/api/img/route.ts
@@ -23,7 +23,7 @@ interface ReportData {
 }
 
 // Endpoint for receiving an image from the client
-export async function POST(req: NextRequest, res: NextResponse): Promise<ReportData> {
+export async function POST(req: NextRequest, res: NextResponse) {
   const blob = await req.blob()
 
   // TODO: Lasse extract data from blob
@@ -31,11 +31,11 @@ export async function POST(req: NextRequest, res: NextResponse): Promise<ReportD
   // TODO: Jens use data from blob to call OpenAI API
 
 
-  return {
+  return NextResponse.json({
     panelId: "123",
     location: { lat: 0, lng: 0 },
     priority: Priority.HIGH,
     category: Category.CRACK,
     description: "Big crack. Doesn't work."
-  }
+  })
 }
