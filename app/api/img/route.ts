bb70ee2078177575d153c40e4e9e39b567b66943
Lasse D. Skaalum <ldsk@trifork.com>
2023-09-29 09:53:15 +0200

    Revert "TO BE REVERTED"
    This reverts commit 28055b6cf3cdd15eeaa4d31e96942beed36c39a5.

--- app/api/img/route.ts
+++ app/api/img/route.ts
@@ -27,14 +27,12 @@ export async function POST(req: NextRequest, res: NextResponse) {
     [description, sapData] = await Promise.all([descriptionPromise, sapDataPromise])
   } catch (e) {
     console.error(e)
-    // return NextResponse.json({ message: "Failed to get SAP data" }, { status: 500 })
-    description = "Hi Hinrik :)"
+    return NextResponse.json({ message: "Failed to get SAP data" }, { status: 500 })
   }
-
+  
   return NextResponse.json({
       paneId,
-      // location: toLocation(sapData),
-      location: [64.13548, -21.89541],
+      location: toLocation(sapData),
       priority: Priority[heighestScoringPrediction.category],
       category: Category[heighestScoringPrediction.category],
       description,
